ad34f1bb143885e341816351c8fbca8fbc1817dc
LeonaKantoo <63060641+Leona07005@users.noreply.github.com>
2021-01-20 18:09:22 -0300

    Add files via upload

---
+++ SkyBlockFarmDeFerro/Form1.Designer.vb
@@ -0,0 +1,102 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Shifter = New System.Windows.Forms.Timer(Me.components)
+        Me.AntiBotter = New System.Windows.Forms.Timer(Me.components)
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(208, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Começar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(12, 41)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(208, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Parar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Shifter
+        '
+        Me.Shifter.Interval = 5000
+        '
+        'AntiBotter
+        '
+        Me.AntiBotter.Interval = 1500
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Red
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 99)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(35, 35)
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(53, 111)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(54, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Desligado"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(232, 161)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Shifter As Timer
+    Friend WithEvents AntiBotter As Timer
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+End Class
---
+++ SkyBlockFarmDeFerro/Form1.vb
@@ -0,0 +1,39 @@
+﻿Public Class Form1
+    Public Declare Sub keybd_event Lib "user32" (ByVal bVk As Byte, ByVal bScan As Byte, ByVal dwFlags As Integer, ByVal dwExtraInfo As Integer)
+
+    Private Const KEYEVENTF_KEYDOWN As Integer = &H0
+    Private Const KEYEVENTF_KEYUP As Integer = &H2
+
+    Private Sub Shifter_Tick(sender As Object, e As EventArgs) Handles Shifter.Tick
+        keybd_event(CByte(Keys.ShiftKey), 0, KEYEVENTF_KEYDOWN, 0)
+        keybd_event(CByte(Keys.ShiftKey), 0, KEYEVENTF_KEYUP, 0)
+        PictureBox1.BackColor = Color.Green
+        Label1.Text = "Ligado"
+    End Sub
+
+    Private Sub AntiBotter_Tick(sender As Object, e As EventArgs) Handles AntiBotter.Tick
+        Dim rng As New Random
+        Select Case rng.Next(1, 5)
+            Case 1
+                My.Computer.Keyboard.SendKeys("W")
+            Case 2
+                My.Computer.Keyboard.SendKeys("A")
+            Case 3
+                My.Computer.Keyboard.SendKeys("S")
+            Case 4
+                My.Computer.Keyboard.SendKeys("D")
+        End Select
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Shifter.Start()
+        AntiBotter.Start()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Shifter.Stop()
+        AntiBotter.Stop()
+        PictureBox1.BackColor = Color.Red
+        Label1.Text = "Desligado"
+    End Sub
+End Class
